7429cb5a66d6bfd039ae32ab60a16aca2564ce76
Flavio Andrade <foa12381@gmail.com>
2023-07-29 19:40:52 -0300

    feat/readfile

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
-import chalk from "chalk";
+import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.bgGreen("Hello my Frends"))
+// console.log(fs);
+console.log(chalk.bgGreen('Hello my Frends'));
+
+export class getFile {
+
+    constructor(path) {
+        this.path = path;
+    }
+
+    handleError(err){
+        console.log(err.message);
+    }
+
+    getFilee(){
+        fs.readFile(this.path, 'utf-8', function(err, data) {
+            if (err) {
+                return this.handleError(err);
+            }
+            console.log(data);
+        });
+    }
+}
+
+const getFile2 = async (path) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(data);
+    });
+};
+
+getFile2('./data/post1.md');
