7108a42f72446f86e3e4f3d587ba067ff2f4725a
Flavio Andrade <foa12381@gmail.com>
2023-07-30 17:46:14 -0300

    feat/cli node --validate

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,43 @@
 import {getFile4} from './main.js';
+import fs from 'fs';
 
 const path = process.argv;
 console.log(path[2], 'helooooo');
 
-getFile4(path[2]);
+
+
+async function processText(args) {
+  const path = args[2];
+  const params = args[3];
+
+  console.log(path, 'helooooo caminho');
+  console.log(params, 'helooooo params');
+
+
+  try {
+    fs.lstatSync(path);
+  } catch (error) {
+    if(error.code === 'ENOENT'){
+      console.log('file or direct no exist');
+      return;
+    }
+  }
+
+  if(fs.lstatSync(path).isFile()) {
+    const result = await getFile4(path);
+    console.log(result);
+
+  }else if (fs.statSync(path).isDirectory()) {
+    const files = await fs.promises.readdir(path);
+    console.log(files);
+    files.forEach(async (file) => {
+      const post = await getFile4(`${path}/${file}`);
+      console.log(file, post);
+    });
+  }
+
+}
+
+
+processText(path);
+
--- src/main.js
+++ src/main.js
@@ -6,35 +6,35 @@ console.log(chalk.bgGreen('Hello my Frends'));
 
 export class getFile {
 
-    constructor(path) {
-        this.path = path;
-    }
+  constructor(path) {
+    this.path = path;
+  }
 
-    handleError(err){
-        throw new Error(chalk.red(err.message));
-    }
+  handleError(err){
+    throw new Error(chalk.red(err.message));
+  }
 
-    getFilee(){
-        fs.readFile(this.path, 'utf-8', function(err, data) {
-            if (err) {
-                this.handleError(err);
-            }
-            console.log(chalk.blue(data));
-        });
-    }
+  getFilee(){
+    fs.readFile(this.path, 'utf-8', function(err, data) {
+      if (err) {
+        this.handleError(err);
+      }
+      console.log(chalk.blue(data));
+    });
+  }
 }
 
 function handleError(err) {
-    throw new Error(chalk.red(err.message));
+  throw new Error(chalk.red(err.message));
 }
 
 export const getFile2 = async (path) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-        if (err) {
-            return handleError(err);
-        }
-        console.log(chalk.green(data));
-    });
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      return handleError(err);
+    }
+    console.log(chalk.green(data));
+  });
 };
 
 // getFile2('./data/post1.md');
@@ -42,31 +42,31 @@ export const getFile2 = async (path) => {
 
 
 export function getFile3(path) {
-    const encoding = 'utf-8';
+  const encoding = 'utf-8';
 
-    fs.promises
-        .readFile(path, encoding)
-        .then(function (data) {
-            console.log('get3',data);
-        }).catch(handleError);
+  fs.promises
+    .readFile(path, encoding)
+    .then(function (data) {
+      console.log('get3',data);
+    }).catch(handleError);
 }
 
 export async function getFile4(path) {
-    const encoding = 'utf-8';
-    try {
-        const result = await fs.promises.readFile(path, encoding);
-        console.log(extraiLinks(result));
-    } catch (error) {
-        handleError(error);
-    } finally {
-        console.log(chalk.yellow('operação concluída'));
-    }
+  const encoding = 'utf-8';
+  try {
+    const result = await fs.promises.readFile(path, encoding);
+    return extraiLinks(result);
+  } catch (error) {
+    handleError(error);
+  } finally {
+    console.log(chalk.yellow('operação concluída'));
+  }
 }
 
 //getFile4('./data/post1.md');
 
 export function extraiLinks(texto) {
-    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const links = [...texto.matchAll(regex)];
-    return links.map((item) => ({[item[1]]: item[2]}));
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const links = [...texto.matchAll(regex)];
+  return links.map((item) => ({[item[1]]: item[2]}));
 }
